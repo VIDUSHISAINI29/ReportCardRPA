e89be460f93fcab64f72acf02269f1c61ed73157
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-12-15 21:52:30 +0530

    more

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,24 +1,37 @@
 <script setup>
 import { ref } from "vue";
-
 import Select from 'primevue/select';
 
 const selectedAttRep = ref();
 const attReport = ref([
-    { name: 'The student has maintained near-perfect attendance, showing a strong commitment to their education and eagerness to learn', code: 'Excellent' },
-    { name: ' The student attends regularly with only a few absences, indicating a responsible attitude toward school', code: 'Good' },
-    { name: 'Attendance is satisfactory, but occasional absences suggest a need for better consistency.', code: 'Satisfactory' },
-    { name: "Frequent absences have slightly impacted the student's learning progress.", code: 'Need Improvemnt' },
-    { name: "Irregular attendance has significantly hindered the student's ability to stay updated with classroom activities.", code: 'Poor' }
+    { name: 'The student has maintained near-perfect attendance, showing a strong commitment to their education and eagerness to learn', code: 'Excellent', bgColor: 'tw-bg-green-500' },
+    { name: 'The student attends regularly with only a few absences, indicating a responsible attitude toward school', code: 'Good', bgColor: 'tw-bg-blue-500' },
+    { name: 'Attendance is satisfactory, but occasional absences suggest a need for better consistency.', code: 'Satisfactory', bgColor: 'tw-bg-yellow-500' },
+    { name: "Frequent absences have slightly impacted the student's learning progress.", code: 'Need Improvement', bgColor: 'tw-bg-orange-500' },
+    { name: "Irregular attendance has significantly hindered the student's ability to stay updated with classroom activities.", code: 'Poor', bgColor: 'tw-bg-red-500' }
 ]);
 </script>
 
 <template>
   <div>
-   <div class="tw-flex tw-p-2 tw-justify-center">
-       <Select v-model="selectedAttRep" editable :options="attReport" optionLabel="name" placeholder="Select a City" class="tw-w-56" />
-   </div>
+    <div class="tw-flex tw-p-2 tw-justify-center">
+      <select class=" tw-p-2 tw-rounded-md tw-translate-y-0 hover:tw-translate-y-100 tw-transition-transform tw-duration-500" name="Attendence Behaviour" id="attBehave">
+        <option v-for="(behaviour, index) in attReport" :key="index" :class="[' tw-h-20 tw-rounded-md', behaviour.bgColor]" value="behaviour">{{ behaviour.name }}</option>
+      </select>
+    </div>
+    <div class="tw-flex tw-p-2 tw-justify-center">
+      <select class=" tw-p-2 tw-rounded-md tw-translate-y-0 hover:tw-translate-y-100 tw-transition-transform tw-duration-500" name="Attendence Behaviour" id="attBehave">
+        <option v-for="(behaviour, index) in attReport" :key="index" :class="[' tw-h-20 tw-rounded-md', behaviour.bgColor]" value="behaviour">{{ behaviour.name }}</option>
+      </select>
+    </div>
+    <div class="tw-flex tw-p-2 tw-justify-center">
+      <select class=" tw-p-2 tw-rounded-md tw-translate-y-0 hover:tw-translate-y-100 tw-transition-transform tw-duration-500" name="Attendence Behaviour" id="attBehave">
+        <option v-for="(behaviour, index) in attReport" :key="index" :class="[' tw-h-20 tw-rounded-md', behaviour.bgColor]" value="behaviour">{{ behaviour.name }}</option>
+      </select>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
