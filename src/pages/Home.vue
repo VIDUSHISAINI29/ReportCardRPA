e4efc1c21a524e9b152264e7deee2476a173f3f6
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-12-26 22:54:17 +0530

    hover effect removed

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,26 +14,31 @@ const attReport = ref([
       name: "The student has maintained near-perfect attendance, consistently showing a strong commitment to their education. They rarely miss class and are always punctual, demonstrating a high level of responsibility. This dedication greatly contributes to their academic success and engagement with the lessons.",
       code: "Excellent",
       bgColor: "tw-bg-green-200 hover:tw-bg-green-400",
+      bgColorCard: "tw-bg-green-400",
    },
    {
       name: "The student attends school regularly with only a few absences, indicating a responsible attitude toward their education. They manage their time well and prioritize school commitments. While occasional absences occur, they ensure minimal disruption to their learning progress.",
       code: "Good",
       bgColor: "tw-bg-blue-200 hover:tw-bg-blue-400",
+      bgColorCard: "tw-bg-blue-400",
    },
    {
       name: "The student’s attendance is satisfactory, but occasional absences suggest a need for better consistency. While they attend most classes, these absences sometimes affect their ability to fully engage with the material. A more consistent attendance pattern could help improve their academic performance.",
       code: "Satisfactory",
       bgColor: "tw-bg-yellow-200 hover:tw-bg-yellow-400",
+      bgColorCard: "tw-bg-yellow-400",
    },
    {
       name: "Frequent absences have slightly impacted the student’s learning progress and understanding of key concepts. While they attend most classes, they miss crucial lessons that affect their performance. Regular attendance would greatly benefit their overall academic achievement.",
       code: "Need Improvement",
       bgColor: "tw-bg-orange-200 hover:tw-bg-orange-400",
+      bgColorCard: "tw-bg-'orange'-400",
    },
    {
       name: "Irregular attendance has significantly hindered the student’s ability to stay updated with classroom activities and lessons. Their frequent absences make it difficult for them to keep up with coursework. It is crucial for the student to improve attendance to ensure continued academic progress.",
       code: "Poor",
       bgColor: "tw-bg-red-200 hover:tw-bg-red-400",
+      bgColorCard: "tw-bg-red-400",
    },
 ]);
 
@@ -41,7 +46,7 @@ function triggerAttDropdown(params) {
    const modifyValue = params.name.slice(0, 30) + "...";
    attPlaceholder.value = modifyValue;
    selectedAttReport.value = params.name;
-   attReportBg.value = params.bgColor
+   attReportBg.value = params.bgColorCard
    showAttOptions.value = false;
 }
 function triggerAttDropdownTrue() {
@@ -63,26 +68,31 @@ const academicReport = ref([
       name: "The student demonstrates exceptional academic skills, consistently exceeding expectations across all subjects. They actively engage in class discussions, complete assignments with precision, and showcase a thorough understanding of complex concepts. Their ability to think critically and solve problems creatively sets them apart.",
       code: "Excellent",
       bgColor: "tw-bg-green-200 hover:tw-bg-green-400",
+      bgColorCard: "tw-bg-green-400",
    },
    {
       name: "The student has a strong academic foundation and performs well in most areas. They actively participate in class and complete assignments on time. Occasionally, they may need support with more advanced topics, but their overall performance reflects dedication and hard work.",
       code: "Good",
       bgColor: "tw-bg-blue-200 hover:tw-bg-blue-400",
+      bgColorCard: "tw-bg-blue-400",
    },
    {
       name: "The student has a satisfactory understanding of the core subjects. While they perform well in familiar topics, they sometimes face challenges in new or complex areas. With consistent effort and additional practice, they have the potential to enhance their skills and performance.",
       code: "Satisfactory",
       bgColor: "tw-bg-yellow-200 hover:tw-bg-yellow-400",
+      bgColorCard: "tw-bg-yellow-400",
    },
    {
       name: "The student struggles with certain subjects and requires focused guidance to improve their academic performance. They may lack confidence in applying concepts and need encouragement to actively participate in class. Regular practice and personalized attention could help them overcome these challenges.",
       code: "Needs Improvement",
       bgColor: "tw-bg-orange-200 hover:tw-bg-orange-400",
+      bgColorCard: "tw-bg-orange-400",
    },
    {
       name: "The student's academic performance is significantly below expectations. They face difficulty understanding fundamental concepts and often need substantial support to complete tasks. A structured intervention plan, combined with consistent encouragement, is essential to help them progress.",
       code: "Poor",
       bgColor: "tw-bg-red-200 hover:tw-bg-red-400",
+      bgColorCard: "tw-bg-red-400",
    },
 ]);
 
@@ -90,7 +100,7 @@ function triggerAcademicDropdown2(params) {
    const modifyValue = params.name.slice(0, 30) + "...";
    academicPlaceholder.value = modifyValue;
    selectedAcademicReport.value = params.name;
-   academicReportBg.value = params.bgColor
+   academicReportBg.value = params.bgColorCard
    showAcademicOptions.value = false;
 }
 function triggerAcademicDropdownTrue2() {
@@ -112,26 +122,31 @@ const exCurricularsReport = ref([
       name: "The student demonstrates exceptional involvement in extracurricular activities, consistently exceeding expectations. They show strong leadership skills, contribute effectively to group tasks, and take initiative in organizing events. Their creativity and passion for extracurriculars inspire others and set a high standard for their peers.",
       code: "Excellent",
       bgColor: "tw-bg-green-200 hover:tw-bg-green-400",
+      bgColorCard: "tw-bg-green-400",
    },
    {
       name: "The student is highly engaged in extracurricular activities and consistently performs well. They contribute meaningfully to group projects, exhibit a positive attitude, and actively participate in events. While they occasionally need support with larger or more complex activities, their overall involvement is impressive.",
       code: "Good",
       bgColor: "tw-bg-blue-200 hover:tw-bg-blue-400",
+      bgColorCard: "tw-bg-blue-400",
    },
    {
       name: "The student participates in extracurricular activities with a solid level of engagement. They contribute positively to group tasks and attend most events. However, they sometimes struggle with new or unfamiliar activities, requiring additional effort and practice to fully excel in these areas.",
       code: "Satisfactory",
       bgColor: "tw-bg-yellow-200 hover:tw-bg-yellow-400",
+      bgColorCard: "tw-bg-yellow-400",
    },
    {
       name: "The student shows limited involvement in extracurricular activities and faces challenges in contributing effectively. They often lack confidence in leadership roles and may find it difficult to work in group settings. With focused guidance and encouragement, they have the potential to improve their participation.",
       code: "Needs Improvement",
       bgColor: "tw-bg-orange-200 hover:tw-bg-orange-400",
+      bgColorCard: "tw-bg-orange-400",
    },
    {
       name: "The student struggles significantly with extracurricular activities and rarely engages in events. They have difficulty taking on tasks or contributing to group efforts and often require substantial support. A more structured plan, along with consistent motivation and encouragement, is necessary to help them become more involved.",
       code: "Poor",
       bgColor: "tw-bg-red-200 hover:tw-bg-red-400",
+      bgColorCard: "tw-bg-red-400",
    },
 ]);
 
@@ -139,7 +154,7 @@ function triggerExCurDropdown2(params) {
    const modifyValue = params.name.slice(0, 30) + "...";
    exCurPlaceholder.value = modifyValue;
    selectedExCurReport.value = params.name;
-   exCurReportBg.value = params.bgColor
+   exCurReportBg.value = params.bgColorCard
    showExCurOptions.value = false;
 }
 function triggerExCurDropdownTrue2() {
@@ -157,26 +172,31 @@ const overallReport = ref([
       name: "The student consistently excels in all subjects, demonstrating outstanding academic performance. They actively participate in class discussions, complete assignments with great detail, and show a high level of intellectual curiosity. Their work is consistently of the highest quality, and they often go above and beyond expectations. This student is a role model for peers and a strong contributor to the school community.",
       code: "Excellent",
       bgColor: "tw-bg-green-200 hover:tw-bg-green-400",
+      bgColorCard: "tw-bg-green-400",
    },
    {
       name: "The student performs well in most subjects, maintaining a strong grasp of the material. They are diligent and consistently submit assignments on time, although there is some room for improvement in certain areas. Overall, they demonstrate a positive attitude toward learning and contribute meaningfully in class. With continued effort, they have the potential to achieve even greater success.",
       code: "Good",
       bgColor: "tw-bg-blue-200 hover:tw-bg-blue-400",
+      bgColorCard: "tw-bg-blue-400",
    },
    {
       name: "The student’s performance is generally satisfactory, though there are some areas that need attention. While they complete most assignments and participate in class, they may occasionally struggle with certain concepts or exhibit inconsistency in their work. Focusing on areas of weakness and maintaining a more consistent effort could help improve their overall academic performance.",
       code: "Satisfactory",
       bgColor: "tw-bg-yellow-200 hover:tw-bg-yellow-400",
+      bgColorCard: "tw-bg-yellow-400",
    },
    {
       name: "The student’s academic performance requires improvement. While they show some understanding of the material, there are frequent gaps in their knowledge and understanding of key concepts. This is often due to inconsistent effort or lack of engagement in class activities. With more focus, participation, and effort, their performance can improve significantly.",
       code: "Need Improvement",
       bgColor: "tw-bg-orange-200 hover:tw-bg-orange-400",
+      bgColorCard: "tw-bg-orange-400",
    },
    {
       name: "The student is struggling academically, with frequent challenges in understanding the material and completing assignments. Their performance is below expectations, often due to lack of consistent effort, participation, and focus. It is essential for the student to seek additional support and adopt a more proactive approach to learning in order to make progress and improve their grades.",
       code: "Poor",
       bgColor: "tw-bg-red-200 hover:tw-bg-red-400",
+      bgColorCard: "tw-bg-red-400",
    },
 ]);
 const overallPlaceholder = ref("Select Overall Performance Record");
@@ -188,7 +208,7 @@ function triggerOverallDropdown2(params) {
    const modifyValue = params.name.slice(0, 30) + "...";
    overallPlaceholder.value = modifyValue;
    selectedOverallReport.value = params.name;
-   overallReportBg.value = params.bgColor
+   overallReportBg.value = params.bgColorCard
    console.log('overall', overallReportBg.value);
    
    showOverallOptions.value = false;
@@ -291,7 +311,7 @@ function triggerOverallDropdownTrue2() {
          <div class="tw-flex tw-w-4/5 tw-flex-col tw-gap-3">
             <div class="tw-w-full tw-p-1 tw-bg-white tw-flex tw-rounded-md tw-px-3 tw-gap-10">
                <div v-for="(category, index) in categoryReport" class="tw-flex tw-items-center tw-gap-2">
-                  <span :class="['tw-w-3 tw-h-3', attReport[index].bgColor]"></span>
+                  <span :class="['tw-w-3 tw-h-3', overallReport[index].bgColorCard]"></span>
                   <span class="tw-font-bold tw-text-sm">{{category}}</span>
                </div>
             </div>
